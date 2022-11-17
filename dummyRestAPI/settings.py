760206b90b14e50c52eeb665bc71a8222cb661df
EmmanuelAddi <emmanuelbett82108@gmail.com>
2022-11-17 16:00:37 +0300

    Dummy API with Django Rest Framework

--- dummyRestAPI/settings.py
+++ dummyRestAPI/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainApp',
     
-    'mainApp'
+    #third party apps
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- viewsets/views.py
+++ viewsets/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(http_method_names=["GET", "POST"])
+def homepage (request:Request):
+    
+    if request.method == "POST":
+        data = request.data
+        
+        response={"message":"Hello World", "data":data}
+        return Response(data=response, status=status.HTTP_201_CREATED)
+    
+    response={"message":"Hello World"}
+    return Response(data=response, status=status.HTTP_200_OK)
+
+
+
